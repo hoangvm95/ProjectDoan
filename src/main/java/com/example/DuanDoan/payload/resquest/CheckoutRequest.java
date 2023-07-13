e40d2b025eecbd2a83b702f53cac2c33b3eec00e
Leo Hoang <hoangvm95@gmail.com>
2023-07-13 19:06:07 +0700

    total

--- src/main/java/com/example/DuanDoan/payload/resquest/CheckoutRequest.java
+++ src/main/java/com/example/DuanDoan/payload/resquest/CheckoutRequest.java
@@ -1,6 +1,13 @@
 package com.example.DuanDoan.payload.resquest;
 
+import javax.validation.constraints.*;
+
 public class CheckoutRequest {
+
+    @NotNull(message = "Vui lòng nhập CMND từ 9-12 ký tự")
+    @Min(9)
+    @Max(12)
+    @Positive
     private String cccd;
     private String locationArrive;
 
